9d5f23d9e1df687b956820d672c5e4416f041c9a
Elias Graciano <eli162004@gmail.com>
2024-08-23 17:00:15 -0300

    ejercicio 1 corregido

--- TrabajoPractico1.py
+++ TrabajoPractico1.py
@@ -1,92 +1,96 @@
 from os import write
-
-tamañoApellido = 16
-tamañoNombre = 16
-tamañoCodigo = 4
-tamañoDato = 36
+import os
+TAMANIOAPELLIDO = 16
+TAMANIONOMBRE = 16
+TAMANIOCODIGO = 4
+TAMANIODATO= TAMANIOCODIGO + TAMANIOAPELLIDO + TAMANIONOMBRE
+PATH = "Archivo.txt"
 """si multiplico 36 por la cantidad de datos,obtengo el tamaño del archivo"""
-"""archivo =open("Archivo.txt","wt")"""
-def altas(apellido,nombre,codigo):
-    with open("Archivo.txt","a",encoding="utf-8") as archivo:
-        archivo.write(apellido.ljust(tamañoApellido))
-        archivo.write(nombre.ljust(tamañoNombre))
-        archivo.write(codigo.ljust(tamañoCodigo))
+def insert(new_apellido,new_nombre,new_codigo):
+    writeByPK(None,new_apellido, new_nombre, new_codigo)
 
-def leerdatos(index):
+
+def readByPK(index):
     pos = offset(index)
-    with open("Archivo.txt","rt",encoding="utf-8") as archivo:
+    return readByOffset(pos)
+
+def readByOffset(pos):
+    with open(PATH,"rt",encoding="utf-8") as archivo:
         archivo.seek(pos)
-        apellido = archivo.read(tamañoApellido).strip()
-        nombre = archivo.read(tamañoNombre).strip()
-        codigo = archivo.read(tamañoCodigo).strip()
+        apellido = archivo.read(TAMANIOAPELLIDO).strip()
+        nombre = archivo.read(TAMANIONOMBRE).strip()
+        codigo = archivo.read(TAMANIOCODIGO).strip()
         return apellido,nombre,codigo
 
-def bajas(index):
+def writeByPK(index,apellido,nombre,codigo):
+    if index is not None:
+        pos = offset(index)
+    else:
+        pos = None
+    writeByOffset(pos,apellido,nombre,codigo)
+
+def writeByOffset(pos,apellido,nombre,codigo):
+    with open(PATH,"r+b") as archivo:
+        if pos is None:
+            archivo.seek(0,2)
+        else:
+            archivo.seek(pos)
+        archivo.write(apellido.ljust(TAMANIOAPELLIDO).encode("utf-8"))
+        archivo.write(nombre.ljust(TAMANIONOMBRE).encode("utf-8"))
+        archivo.write(codigo.ljust(TAMANIOCODIGO).encode("utf-8"))
+
+def delete(index):
+    #corregido
     pos = offset(index)
-    with open("Archivo.txt","r+",encoding="utf-8") as archivo:
-        archivo.seek(pos + tamañoDato)
+    with open(PATH,"r+b") as archivo:
+        archivo.seek(-TAMANIODATO,2)
         resto = archivo.read()
         archivo.seek(pos)
         archivo.write(resto)
+        archivo.seek(-TAMANIODATO,2)
         archivo.truncate()
 
 def offset(index):
-    offset1 = tamañoDato * (index-1)
+    offset1 = TAMANIODATO * (index-1)
     return offset1
 
-def modificacion(index, apellido, nombre, codigo):
+def update(index, new_apellido, new_nombre, new_codigo):
     pos = offset(index)
-    with open('Archivo.txt', 'r+b') as archivo:
-        archivo.seek(pos)
+    old_apellido,old_nombre,old_codigo = readByPK(index)
+    if new_apellido is None:
+        new_apellido = old_apellido
 
-        if apellido is not None:
-            apellido = apellido.ljust(tamañoApellido)[:tamañoApellido]
-            archivo.write(apellido.encode('utf-8'))
-        else:
-            archivo.seek(tamañoApellido, 1)
+    if new_nombre is None:
+        new_nombre = old_nombre
 
-        if nombre is not None:
-            nombre = nombre.ljust(tamañoNombre)[:tamañoNombre]
-            archivo.write(nombre.encode('utf-8'))
-        else:
-            archivo.seek(tamañoNombre, 1)
+    if new_codigo is None:
+        new_codigo = old_codigo
 
-        if codigo is not None:
-            codigo = codigo.ljust(tamañoCodigo)[:tamañoCodigo]
-            archivo.write(codigo.encode('utf-8'))
-        else:
-            archivo.seek(tamañoCodigo, 1)
+    writeByPK(index,new_apellido,new_nombre,new_codigo)
+
 
 
 def listaClientes():
-    with open("Archivo.txt","rt") as archivo:
-        archivo.seek(0, 2)  #paro el cursor al final para calcular el tamaño
-        tamanio = archivo.tell()
-        archivo.seek(0) # paro el cursor al principio despues de calcular el tamaño
-        i = 0
-        j = 1
-        elementos = (tamanio / tamañoDato)
-        while i < elementos:
-            apellido = archivo.read(tamañoApellido).strip()
-            nombre = archivo.read(tamañoNombre).strip()
-            codigo = archivo.read(tamañoCodigo).strip()
-            print("Cliente ", j)
-            print("Apellido: ", apellido)
-            print("Nombre: ", nombre)
-            print("Código: ", codigo)
-            print("----------------------")
-            i += 1
-            j +=1
-
+    index = 1
+    while True:
+        apellido,nombre,codigo = readByPK(index)
+        if not codigo:
+            break
+        print("Cliente ", index)
+        print("Apellido: ", apellido)
+        print("Nombre: ", nombre)
+        print("Código: ", codigo)
+        print("----------------------")
+        index += 1
 
 def main():
     while True:
         print("\nMenú de Opciones")
+        print("0. Salir")
         print("1. Cargar datos (Alta)")
         print("2. Eliminar datos (Baja)")
         print("3. Modificar datos")
         print("4. Mostrar Lista de Clientes")
-        print("5. Salir")
 
         opcion = input("Selecciona una opción: ")
 
@@ -95,19 +99,19 @@ def main():
             apellido = input("Ingrese el apellido: ")
             nombre = input("Ingrese el nombre: ")
             codigo = input("Ingrese el código: ")
-            altas(apellido, nombre, codigo)
+            insert(apellido, nombre, codigo)
             print("Registro agregado correctamente.")
 
         elif opcion == '2':
             # Eliminar datos
             index = int(input("Ingrese el índice del registro a eliminar: "))
-            bajas(index)
+            delete(index)
             print(f"Registro en índice {index} eliminado correctamente.")
 
         elif opcion == '3':
             # Modificar datos
             index = int(input("Ingrese el índice del registro a modificar: "))
-            apellidoaux,nombreaux,codigoaux = leerdatos(index)
+            apellidoaux,nombreaux,codigoaux = readByPK(index)
             print("Datos a modificar: ")
             print("apellido: ",apellidoaux)
             print("Nombre: ",nombreaux)
@@ -121,27 +125,21 @@ def main():
             nombre = nombre if nombre != "" else None
             codigo = codigo if codigo != "" else None
 
-            modificacion(index, apellido, nombre, codigo)
+            update(index, apellido, nombre, codigo)
             print(f"Registro en índice {index} modificado correctamente.")
-        #
-        # elif opcion == '4':
-        #     # Leer datos
-        #     index = int(input("Ingrese el índice del registro a leer: "))
-        #     apellido, nombre, codigo = leerdatos(index)
-        #     print(f"Registro en índice {index}: Apellido: {apellido}, Nombre: {nombre}, Código: {codigo}")
 
         elif opcion == '4':
             print("Listado de Clientas:")
             print("----------------------")
             listaClientes()
 
-        elif opcion == '5':
+        elif opcion == '0':
             # Salir
             print("Saliendo del programa...")
             break
 
         else:
-            print("Opción no válida. Por favor, elija una opción del 1 al 5.")
+            print("Opción no válida. Por favor, elija una opción del 0 al 4.")
     return
 
 main()
