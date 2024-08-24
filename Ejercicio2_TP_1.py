9cdf28117bb7bb0a69e54bef6a21337d42b31f92
Elias Graciano <eli162004@gmail.com>
2024-08-24 16:54:16 -0300

    no puedo

--- Ejercicio2_TP_1.py
+++ Ejercicio2_TP_1.py
@@ -57,13 +57,13 @@ def readByOffset(pos):
 
 def writeByPK(new_apellido,new_nombre,new_codigo):
     pos = getoffset(new_codigo)
-    new_registro = readByPK(new_codigo)
-    if new_registro is not None:
+    old_registro = readByPK(new_codigo)
+    if old_registro is not None:
         if readOverflow(new_codigo)[2] != "":                       # if readByOffset(new_codigo)[2] != "":
             writeinOverflow(new_apellido,new_nombre,new_codigo)     # writeinOverflow(new_apellido,new_nombre,new_codigo)
         else:
             #le tengo que pasar un codigo que sea : ""
-            writeByOffset(new_apellido,new_nombre,new_codigo)
+            writeByOffset(pos,new_apellido,new_nombre,new_codigo)
     else:
         return  None
 
@@ -91,11 +91,17 @@ def update(new_apellido, new_nombre, new_codigo):
         new_nombre = old_nombre
 
     if new_codigo is None:
-        new_codigo = "".ljust(TAMANIOCODIGO)
+        new_codigo = old_codigo
 
+    writeByPK(new_apellido,new_nombre,new_codigo)
 
-    writeByPK(pos,new_apellido,new_nombre,new_codigo)
-
+def mostrarcliente(codigo):
+    apellido,nombre,old_codigo = readByPK(codigo)
+    print("----------------------")
+    print("Apellido: ", apellido)
+    print("Nombre: ", nombre)
+    print("Código: ", old_codigo)
+    print("----------------------")
 
 
 def listaClientes():
@@ -112,6 +118,7 @@ def listaClientes():
         index += 1
 
 def main():
+    #crearArchivo()
     while True:
         print("\nMenú de Opciones")
         print("0. Salir")
@@ -119,6 +126,7 @@ def main():
         print("2. Eliminar datos (Baja)")
         print("3. Modificar datos")
         print("4. Mostrar Lista de Clientes")
+        print("5. Mostrar cliente")
 
         opcion = input("Selecciona una opción: ")
 
@@ -138,8 +146,8 @@ def main():
 
         elif opcion == '3':
             # Modificar datos
-            index = int(input("Ingrese el índice del registro a modificar: "))
-            apellidoaux,nombreaux,codigoaux = readByPK(index)
+            codigo = int(input("Ingrese el codigo del registro a modificar: "))
+            apellidoaux,nombreaux,codigoaux = readByPK(codigo)
             print("Datos a modificar: ")
             print("apellido: ",apellidoaux)
             print("Nombre: ",nombreaux)
@@ -153,13 +161,17 @@ def main():
             nombre = nombre if nombre != "" else None
             codigo = codigo if codigo != "" else None
 
-            update(index, apellido, nombre, codigo)
+            update(apellido, nombre, codigo)
             print(f"Registro en índice {index} modificado correctamente.")
 
         elif opcion == '4':
             print("Listado de Clientas:")
             print("----------------------")
             listaClientes()
+
+        elif opcion == '5':
+            codigo = input("ingrese codigo: ")
+            mostrarcliente(codigo)
 
         elif opcion == '0':
             # Salir
@@ -168,6 +180,7 @@ def main():
 
         else:
             print("Opción no válida. Por favor, elija una opción del 0 al 4.")
+
     return
 
 main()
